1c18f851188ec129fea88736e75dc6ba62cf2238
Mateo Calle Molina <matecalle@gmail.com>
2019-07-16 20:16:27 -0500

    Completes the IMDB project

---
+++ IMDB-keras/trainingplot.py
@@ -0,0 +1,52 @@
+import matplotlib
+
+# Specifying the backend to be used before importing pyplot
+# to avoid "RuntimeError: Invalid DISPLAY variable"
+# matplotlib.use('agg')
+import matplotlib.pyplot as plt
+import tensorflow.keras
+import numpy as np
+
+class TrainingPlot(tensorflow.keras.callbacks.Callback):
+
+    def on_train_batch_begin(self, *args, **kwargs):
+        pass
+
+    # This function is called when the training begins
+    def on_train_begin(self, logs={}):
+        # Initialize the lists for holding the logs, losses and accuracies
+        self.losses = []
+        self.acc = []
+        self.val_losses = []
+        self.val_acc = []
+        self.logs = []
+
+    # This function is called at the end of each epoch
+    def on_epoch_end(self, epoch, logs={}):
+
+        # Append the logs, losses and accuracies to the lists
+        self.logs.append(logs)
+        self.losses.append(logs.get('loss'))
+        self.acc.append(logs.get('accuracy'))
+        self.val_losses.append(logs.get('val_loss'))
+        self.val_acc.append(logs.get('val_accuracy'))
+
+        # Before plotting ensure at least 2 epochs have passed
+        if len(self.losses) > 1:
+
+            N = np.arange(0, len(self.losses))
+
+            # You can chose the style of your preference
+            # print(plt.style.available) to see the available options
+            #plt.style.use("seaborn")
+
+            # Plot train loss, train acc, val loss and val acc against epochs passed
+            plt.figure()
+            plt.plot(N, self.losses, label = "train_loss")
+            plt.plot(N, self.acc, label = "train_accuracy")
+            plt.plot(N, self.val_losses, label = "val_loss")
+            plt.plot(N, self.val_acc, label = "val_accuracy")
+            plt.title("Training Loss and Accuracy [Epoch {}]".format(epoch))
+            plt.xlabel("Epoch #")
+            plt.ylabel("Loss/Accuracy")
+            plt.legend()
